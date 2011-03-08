79b8d94afb7cb031d42d6bfb4cbf247cfd10189f
Paul Taylor <ijabz@fastmail.fm>
2011-03-08 15:05:02 +0000

    Updated xsd from latest relax schema

--- brainz-mmd2-jaxb/src/main/java/org/musicbrainz/mmd2/AttributeList.java
+++ brainz-mmd2-jaxb/src/main/java/org/musicbrainz/mmd2/AttributeList.java
@@ -8,17 +8,12 @@
 
 package org.musicbrainz.mmd2;
 
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-import javax.xml.bind.JAXBElement;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlElementRef;
-import javax.xml.bind.annotation.XmlMixed;
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlSchemaType;
 import javax.xml.bind.annotation.XmlType;
 
 
@@ -32,9 +27,8 @@ import javax.xml.bind.annotation.XmlType;
  *   &lt;complexContent>
  *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
  *       &lt;sequence>
- *         &lt;element ref="{http://musicbrainz.org/ns/mmd-2.0#}script" minOccurs="0"/>
+ *         &lt;element ref="{http://musicbrainz.org/ns/mmd-2.0#}attribute" maxOccurs="unbounded"/>
  *       &lt;/sequence>
- *       &lt;attribute name="type" type="{http://www.w3.org/2001/XMLSchema}anyURI" />
  *     &lt;/restriction>
  *   &lt;/complexContent>
  * &lt;/complexType>
@@ -44,70 +38,41 @@ import javax.xml.bind.annotation.XmlType;
  */
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "", propOrder = {
-    "content"
+    "attribute"
 })
-@XmlRootElement(name = "alias")
-public class Alias {
+@XmlRootElement(name = "attribute-list")
+public class AttributeList {
 
-    @XmlElementRef(name = "script", namespace = "http://musicbrainz.org/ns/mmd-2.0#", type = JAXBElement.class)
-    @XmlMixed
-    protected List<Serializable> content;
-    @XmlAttribute
-    @XmlSchemaType(name = "anyURI")
-    protected String type;
+    @XmlElement(required = true)
+    protected List<String> attribute;
 
     /**
-     * Gets the value of the content property.
+     * Gets the value of the attribute property.
      * 
      * <p>
      * This accessor method returns a reference to the live list,
      * not a snapshot. Therefore any modification you make to the
      * returned list will be present inside the JAXB object.
-     * This is why there is not a <CODE>set</CODE> method for the content property.
+     * This is why there is not a <CODE>set</CODE> method for the attribute property.
      * 
      * <p>
      * For example, to add a new item, do as follows:
      * <pre>
-     *    getContent().add(newItem);
+     *    getAttribute().add(newItem);
      * </pre>
      * 
      * 
      * <p>
      * Objects of the following type(s) are allowed in the list
-     * {@link JAXBElement }{@code <}{@link String }{@code >}
      * {@link String }
      * 
      * 
      */
-    public List<Serializable> getContent() {
-        if (content == null) {
-            content = new ArrayList<Serializable>();
+    public List<String> getAttribute() {
+        if (attribute == null) {
+            attribute = new ArrayList<String>();
         }
-        return this.content;
-    }
-
-    /**
-     * Gets the value of the type property.
-     * 
-     * @return
-     *     possible object is
-     *     {@link String }
-     *     
-     */
-    public String getType() {
-        return type;
-    }
-
-    /**
-     * Sets the value of the type property.
-     * 
-     * @param value
-     *     allowed object is
-     *     {@link String }
-     *     
-     */
-    public void setType(String value) {
-        this.type = value;
+        return this.attribute;
     }
 
 }
--- brainz-mmd2-jaxb/src/main/java/org/musicbrainz/mmd2/DefQuality.java
+++ brainz-mmd2-jaxb/src/main/java/org/musicbrainz/mmd2/DefQuality.java
@@ -14,34 +14,34 @@ import javax.xml.bind.annotation.XmlType;
 
 
 /**
- * <p>Java class for def_direction.
+ * <p>Java class for def_quality.
  * 
  * <p>The following schema fragment specifies the expected content contained within this class.
  * <p>
  * <pre>
- * &lt;simpleType name="def_direction">
+ * &lt;simpleType name="def_quality">
  *   &lt;restriction base="{http://www.w3.org/2001/XMLSchema}token">
- *     &lt;enumeration value="both"/>
- *     &lt;enumeration value="forward"/>
- *     &lt;enumeration value="backward"/>
+ *     &lt;enumeration value="low"/>
+ *     &lt;enumeration value="normal"/>
+ *     &lt;enumeration value="high"/>
  *   &lt;/restriction>
  * &lt;/simpleType>
  * </pre>
  * 
  */
-@XmlType(name = "def_direction")
+@XmlType(name = "def_quality")
 @XmlEnum
-public enum DefDirection {
+public enum DefQuality {
 
-    @XmlEnumValue("both")
-    BOTH("both"),
-    @XmlEnumValue("forward")
-    FORWARD("forward"),
-    @XmlEnumValue("backward")
-    BACKWARD("backward");
+    @XmlEnumValue("low")
+    LOW("low"),
+    @XmlEnumValue("normal")
+    NORMAL("normal"),
+    @XmlEnumValue("high")
+    HIGH("high");
     private final String value;
 
-    DefDirection(String v) {
+    DefQuality(String v) {
         value = v;
     }
 
@@ -49,8 +49,8 @@ public enum DefDirection {
         return value;
     }
 
-    public static DefDirection fromValue(String v) {
-        for (DefDirection c: DefDirection.values()) {
+    public static DefQuality fromValue(String v) {
+        for (DefQuality c: DefQuality.values()) {
             if (c.value.equals(v)) {
                 return c;
             }
